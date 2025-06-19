94668645c7e672fac82d6285b9b0ae60e5202c56
Rodriguesss22 <playaxieinfinity2021@gmail.com>
2025-06-19 13:21:52 -0300

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1750350110101

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3fe8427f._.js",
+  "static/chunks/src_5d0899be._.js"
+],
+    source: "dynamic"
+});
